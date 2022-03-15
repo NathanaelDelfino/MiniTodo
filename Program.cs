d87068f3e081c71dca2960945a523a10ed21e357
NathanaelDelfino <nathanael.delfino@live.com>
2022-03-15 10:54:52 -0300

    :fire: Removido codigo comentado da rota de post do todo

--- Program.cs
+++ Program.cs
@@ -15,28 +15,5 @@ app.UseSwaggerUI();
 
 app.MapGet("/", () => "Versão 1.0");
 
-
-// app.MapPost("/v1/todos/{id:int}", (DbAppContext context,
-//                                    UpdateTodoViewModel model,
-//                                    int id) =>
-// {
-//     if (id <= 0)
-//         return Results.BadRequest("É necessário um id de uma tarefa");
-
-//     var estado = model.MapTo();
-
-//     var todo = context.Todos.Where(x => x.Id == id).FirstOrDefault();
-
-//     if (todo == null)
-//         return Results.BadRequest("Id não encontrada");
-
-
-//     todo.AsFinished(estado);
-//     context.Todos.Update(todo);
-//     context.SaveChanges();
-//     return todo is not null ? Results.Ok(todo) : Results.NotFound();
-
-// });
-
 app.Run();
 
